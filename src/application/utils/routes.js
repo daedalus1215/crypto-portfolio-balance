5e727c624a222f42200a320835479c8f78b87d1e
larry adams <ladams1776@yahoo.com>
2021-02-15 21:51:07 -0500

    begin to add the pieces for getHistoricalData

--- src/application/utils/routes.js
+++ src/application/utils/routes.js
@@ -1,6 +1,7 @@
 const routes = (app) => {
     app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
     app.head('/status', (req, res) => { res.status(200).end(); });
+    app.get('/api/history/:currency')
 }
 
 
