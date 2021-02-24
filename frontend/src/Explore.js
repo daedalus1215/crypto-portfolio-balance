19abbf0d35cf116a74812eb7cd1e2b822fdbe794
larry adams <ladams1776@yahoo.com>
2021-02-24 08:41:18 -0500

    Update Explore to allow toggle-able range of data.

--- frontend/src/Explore.js
+++ frontend/src/Explore.js
@@ -1,31 +1,81 @@
-import React, { useEffect, useState } from "react";
-import { Line } from "react-chartjs-2";
-import "./HistoricRatesPage.css";
+import React from "react";
 import Button from "react-bootstrap/esm/Button";
+import { Line } from "react-chartjs-2";
+import { getHistoricStuff } from "./requests";
 import useFetchPortfolioWithTotal from "./useFetchPortfolioWithTotal";
+import "./HistoricRatesPage.css";
+
+const TIME_LAPSE = {
+    YR: "YR",
+    MTH: "MTH",
+}
 
 function Explore() {
+    const [timePeriod, setTimePeriod] = React.useState(TIME_LAPSE.YR);
     const [data, setData] = React.useState({});
-    const [hData, setHData] = useState([]);
-    const { portfolio, total } = useFetchPortfolioWithTotal();
-    console.log('explore: ', portfolio);
-    useEffect(() => {
-        
-        setHData(portfolio);
+    const [portfolioData, setPortfolioData] = React.useState([]);
+    const [btcHistory, setBtcHistory] = React.useState([]);
+    const { portfolio } = useFetchPortfolioWithTotal();
+
+    React.useEffect(() => {
+        setPortfolioData(portfolio);
     }, [portfolio]);
 
-    useEffect(() => {
-        const labels = hData.map(timeUnit => {
-            return timeUnit.timestamp;
+    React.useEffect(() => {
+        getHistoricStuff(setBtcHistory);
+    }, []);
+
+
+    React.useEffect(() => {
+        let lastSatoshis = 0;
+        const computeCurrentHoldingValue = (satoshis, rate) => {
+            console.log('satoshis', satoshis);
+
+            lastSatoshis += lastSatoshis + +satoshis;
+
+            return lastSatoshis * (rate / 100000000);
+        }
+        let labels = btcHistory?.map(btc => btc.timestamp);
+
+        let totalAmount = btcHistory.map(btc => {
+            const tAmount = portfolio
+                .filter(folio => {
+                    if (folio.timestamp === btc.timestamp) {
+                        console.log('true!');
+                        console.log(folio.timestamp);
+                        console.log(btc.timestamp);
+                        return true;
+                    } else {
+                        console.log('false!');
+                        console.log(folio.timestamp);
+                        console.log(btc.timestamp);
+                        return false;
+                    }
+                })
+                .map(folio => {
+                    console.log('portfolio amount this time: ', folio.amount);
+                    return folio.amount;
+                })
+                .reduce((first, second) => {
+                    console.log('first', first);
+                    console.log('second', second);
+                    console.log('combined', +first + +second);
+                    return +first + +second
+                }, 0);
+            return computeCurrentHoldingValue(tAmount, btc.rate);
         });
 
-        
+        if (timePeriod === TIME_LAPSE.MTH) {
+            totalAmount = totalAmount.splice(totalAmount.length - 20, totalAmount.length - 1);
+            labels = labels.splice(labels.length - 20, labels.length - 1);
+        }
 
+        console.log('timePeriod equals', totalAmount);
         const lineGraphData = {
             labels,
             datasets: [
                 {
-                    data: hData.map(timeUnit => timeUnit.amount),
+                    data: totalAmount,
                     label: 'Explore',
                     borderColor: "#FFD700",
                     fill: false,
@@ -33,11 +83,11 @@ function Explore() {
             ],
         };
         setData(lineGraphData);
-    }, [hData]);
+    }, [portfolioData, btcHistory, timePeriod]);
 
     return (
         <div className="historic-rates-page">
-            <div><Button>1Y</Button><Button>1M</Button></div>
+            <div><Button onClick={() => setTimePeriod(TIME_LAPSE.YR)}>1Y</Button><Button onClick={() => setTimePeriod(TIME_LAPSE.MTH)}>Month</Button></div>
             <br />
             <div className="grid">
                 <Line data={data} />
@@ -45,4 +95,5 @@ function Explore() {
         </div>
     );
 }
+
 export default Explore;
