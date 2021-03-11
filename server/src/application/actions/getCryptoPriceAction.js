46f9e0b52cba191255196f3ea0099e10df36eae2
larry adams <ladams1776@yahoo.com>
2021-03-10 21:38:46 -0500

    Rename getCryptoPriceAction Because we will pass up the argument

--- server/src/application/actions/getCryptoPriceAction.js
+++ server/src/application/actions/getCryptoPriceAction.js
@@ -2,8 +2,7 @@ const axios = require('axios');
 const key = process.env.NOMIC_API;
 const NOMICS_URL = "https://api.nomics.com/v1/exchange-rates/history?key=";
 
-const getBitcoinPriceAction = () => (req, response) => {
-    console.log('getHistoricalDataByDayAction: ');
+const getCryptoPriceAction = () => (req, response) => {
     const currency = req.params.currency;
     const url = `${NOMICS_URL}${key}&currency=${currency}&start=2020-01-01T00%3A00%3A00Z`;
 
@@ -13,4 +12,4 @@ const getBitcoinPriceAction = () => (req, response) => {
         .catch(err => { response.send('Error fetching data from nomics ' + err); });
 };
 
-module.exports = getBitcoinPriceAction;
+module.exports = getCryptoPriceAction;
--- server/src/application/utils/routes.js
+++ server/src/application/utils/routes.js
@@ -1,10 +1,10 @@
-const getBitcoinPriceAction = require('../actions/getBitcoinPriceAction');
+const getCryptoPriceAction = require('../actions/getCryptoPriceAction');
 
 const routes = (app, key) => {
-    const getBitcoinPrice = getBitcoinPriceAction(key);
+    const getCryptoPriceRequestHandler = getCryptoPriceAction(key);
     app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
     app.head('/status', (req, res) => { res.status(200).end(); });
-    app.get('/api/history/:currency', getBitcoinPrice)
+    app.get('/api/history/:currency', getCryptoPriceRequestHandler)
 }
 
 module.exports = routes;
