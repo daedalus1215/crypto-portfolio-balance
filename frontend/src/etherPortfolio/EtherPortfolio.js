a3412ab56d3d87185873f94ee61d592193e5f039
larry adams <ladams1776@yahoo.com>
2021-03-14 21:12:56 -0400

    Update ether portfolio (add more buttons)

--- frontend/src/etherPortfolio/EtherPortfolio.js
+++ frontend/src/etherPortfolio/EtherPortfolio.js
@@ -8,6 +8,8 @@ import "./EtherPortfolio.css";
 const TIME_LAPSE = {
     YR: "YR",
     MTH: "MTH",
+    WEEK: "WEEK",
+    THREE_MTH: "THREE_MTH",
 }
 
 const useSetPortfolioData = (setPortfolioData, portfolio) => {
@@ -26,43 +28,57 @@ const useDisplayHistoricalExchangeRate = (setData, portfolioData, etherHistory, 
 
     React.useEffect(() => {
         const sumMultiplePurchasesInSameDay = (btc) => {
-            console.log('what is this', portfolio)
-            console.log('what is this btc:', btc)
+            // console.log('what is this', portfolio)
+            // console.log('what is this btc:', btc)
             return portfolio?.filter(folio => (folio.timestamp === btc.timestamp)
-                    ? true
-                    : false)
-                .map(folio => folio.amount)
+                ? true
+                : false)
+                .map(folio => parseFloat(folio.amount))
                 .reduce((first, second) => {
                     console.log('first: ', first);
                     console.log('second: ', second);
-                    const total = +first + +second;
-                    console.log('fiatInvestment: ', total)
+                    const total = parseFloat(first + second);
+                    console.log('ethereum total for the day: ', total)
                     return total;
                 }, 0);
         }
 
-        let lastwei = 0;
-        const computeCurrentHoldingValue = (wei, rate) => {
-            lastwei = lastwei + +wei;
-            const weiRate = rate / 100000000;
-            const marketValueForwei = (lastwei * weiRate);
-            return marketValueForwei;
+        let lastEther = 0;
+        const computeCurrentHoldingValue = (ether, rate) => {
+            lastEther = isNaN(lastEther) ? 0 : lastEther
+            console.log('lastEther:', lastEther)
+            console.log('ether:', ether)
+            lastEther = parseFloat(lastEther) + parseFloat(ether);
+            console.log('new lastEther:', lastEther)
+            const etherRate = rate * lastEther;
+            const marketValueForether = etherRate
+            return marketValueForether;
         }
 
         let labels = etherHistory?.map(btc => btc.timestamp);
         console.log('labels')
         let totalAmount = etherHistory
             .map(btc => {
-                const totalAmountOfweiInTheSameDay = sumMultiplePurchasesInSameDay(btc);
-                console.log('totalAmountOfweiInTheSameDay', totalAmountOfweiInTheSameDay)
-                const computedCurrentHoldingValue = computeCurrentHoldingValue(totalAmountOfweiInTheSameDay, btc.rate);
-                console.log('computedCurrentHoldingValue', computedCurrentHoldingValue)
+                const totalAmountOfetherInTheSameDay = sumMultiplePurchasesInSameDay(btc);
+                // console.log('totalAmountOfetherInTheSameDay', totalAmountOfetherInTheSameDay)
+                const computedCurrentHoldingValue = computeCurrentHoldingValue(totalAmountOfetherInTheSameDay, btc.rate);
+                // console.log('computedCurrentHoldingValue', computedCurrentHoldingValue)
                 return computedCurrentHoldingValue
             });
+
+        if (timePeriod === TIME_LAPSE.WEEK) {
+            totalAmount = totalAmount.splice(totalAmount.length - 10, totalAmount.length - 1);
+            labels = labels.splice(labels.length - 10, labels.length - 1);
+        }
 
         if (timePeriod === TIME_LAPSE.MTH) {
             totalAmount = totalAmount.splice(totalAmount.length - 34, totalAmount.length - 1);
             labels = labels.splice(labels.length - 34, labels.length - 1);
+        }
+
+        if (timePeriod === TIME_LAPSE.THREE_MTH) {
+            totalAmount = totalAmount.splice(totalAmount.length - 90, totalAmount.length - 1);
+            labels = labels.splice(labels.length - 90, labels.length - 1);
         }
 
         setTotalValue(totalAmount[totalAmount.length - 1]?.toFixed(2));
@@ -71,7 +87,7 @@ const useDisplayHistoricalExchangeRate = (setData, portfolioData, etherHistory, 
             datasets: [
                 {
                     data: totalAmount,
-                    label: 'Ethereum',
+                    label: 'Value in Ethereum',
                     borderColor: "#77f",
                     fill: false,
                 },
@@ -87,7 +103,7 @@ function EtherPortfolio() {
     const [portfolioData, setPortfolioData] = React.useState([]);
     const [etherHistory, setEtherHistory] = React.useState([]);
     const [totalValue, setTotalValue] = React.useState(0);
-    const ether  = useFetchEther();
+    const { ether, fiatInvestment } = useFetchEther();
     const portfolio = ether;
 
     useSetPortfolioData(setPortfolioData, portfolioData);
@@ -99,11 +115,18 @@ function EtherPortfolio() {
 
     return (
         <div className="historic-rates-page">
-            <div><Button onClick={() => setTimePeriod(TIME_LAPSE.YR)}>Year</Button><Button onClick={() => setTimePeriod(TIME_LAPSE.MTH)}>Month</Button></div>
-            <br />
             <div>
-                {/* <p>Cash invested: ${fiatInvestment.toFixed(2)}</p> */}
-                <p>Valued at: ${totalValue}</p>
+                <Button onClick={() => setTimePeriod(TIME_LAPSE.YR)}>Year</Button>
+                <Button onClick={() => setTimePeriod(TIME_LAPSE.THREE_MTH)}>Three Month</Button>
+                <Button onClick={() => setTimePeriod(TIME_LAPSE.MTH)}>Month</Button>
+                <Button onClick={() => setTimePeriod(TIME_LAPSE.WEEK)}>Week</Button>
+            </div>
+            <br />
+            <div className="title-container">
+                <div className="title">
+                    <p>Invested: ${fiatInvestment.toFixed(2)}</p>
+                    <p>Valued at: ${totalValue}</p>
+                </div>
             </div>
             <div className="grid">
                 <Line data={data} />
