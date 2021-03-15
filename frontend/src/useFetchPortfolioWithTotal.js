8fc948fe4735ee85b8e9500df9795af61edadd24
larry adams <ladams1776@yahoo.com>
2021-03-14 21:12:29 -0400

    update useFetchPortfolioWithTotal remove console and make it more terse

--- frontend/src/useFetchPortfolioWithTotal.js
+++ frontend/src/useFetchPortfolioWithTotal.js
@@ -4,14 +4,9 @@ const portfolio = require('./temp/portfolio.json');
 const useFetchPortfolioWithTotal = () => {
     return React.useMemo(() => {
         const fiatInvestment = portfolio
-            .map(p => p.order)
-            .reduce((sum, order) => {
-                console.log('order', order)
-                console.log('sum', sum)
-                return parseFloat(order) + parseFloat(sum)
-            }, 0);
+            .map(p => parseFloat(p.order))
+            .reduce((sum, order) => order + sum, 0);
 
-        console.log('the total is: ', fiatInvestment);
         return {
             portfolio,
             fiatInvestment
