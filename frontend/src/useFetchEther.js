d14bdfcda28e10474ce0f5047312a1d6a4343e05
larry adams <ladams1776@yahoo.com>
2021-03-14 21:12:00 -0400

    update useFetchEther to give fiat initial invesment like btc portfolio. So much of this is overlap, we can dynamically set these charts in a much better way.

--- frontend/src/useFetchEther.js
+++ frontend/src/useFetchEther.js
@@ -1,5 +1,14 @@
 const ether = require('./temp/ether.json');
 
-const useFetchEther = () => ether;
+const useFetchEther = () => {
+    const fiatInvestment = ether
+        .map(p => parseFloat(p.order))
+        .reduce((sum, order) => order + sum, 0);
+
+    return {
+        ether,
+        fiatInvestment
+    };
+};
 
 export default useFetchEther;
