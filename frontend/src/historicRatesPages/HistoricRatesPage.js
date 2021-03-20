6d2114a00edf9b7988d91b29439415ab8b9691cf
larry adams <ladams1776@yahoo.com>
2021-03-20 18:22:48 -0400

    Add buttons to HistoricRatesPage

--- frontend/src/historicRatesPages/HistoricRatesPage.js
+++ frontend/src/historicRatesPages/HistoricRatesPage.js
@@ -4,26 +4,41 @@ import { Line } from "react-chartjs-2";
 import Button from "react-bootstrap/esm/Button";
 import "./HistoricRatesPage.css";
 
+const TIME_LAPSE = {
+    YR: "YR",
+    MTH: "MTH",
+    WEEK: "WEEK",
+    THREE_MTH: "THREE_MTH",
+}
+
 function HistoricRatesPage() {
     const [data, setData] = React.useState({});
     const [btcHistory, setBtcHistory] = useState([]);
+    const [timePeriod, setTimePeriod] = React.useState(TIME_LAPSE.MTH);
 
     useEffect(() => {
         fetchBitcoinHistory(setBtcHistory);
     }, []);
 
     useEffect(() => {
-        const labels = btcHistory.map(timeUnit => {
-            return timeUnit.timestamp;
-            // const date = timeUnit.timestamp);
-            // return (date.getMonth() + 1) + "/" + date.getDay() + "/" + date.getFullYear();
-        });
+        let labels = btcHistory.map(timeUnit => timeUnit.timestamp);
+        let data = btcHistory.map(timeUnit => timeUnit.rate);
+
+        if (timePeriod === TIME_LAPSE.MTH) {
+            data = data.splice(data.length - 34, data.length - 1);
+            labels = labels.splice(labels.length - 34, labels.length - 1);
+        }
+
+        if (timePeriod === TIME_LAPSE.THREE_MTH) {
+            data = data.splice(data.length - 90, data.length - 1);
+            labels = labels.splice(labels.length - 90, labels.length - 1);
+        }
 
         const lineGraphData = {
             labels,
             datasets: [
                 {
-                    data: btcHistory.map(timeUnit => timeUnit.rate),
+                    data: data,
                     label: 'Bitcoin',
                     borderColor: "#FFD700",
                     fill: false,
@@ -31,11 +46,13 @@ function HistoricRatesPage() {
             ],
         };
         setData(lineGraphData);
-    }, [btcHistory]);
+    }, [btcHistory, timePeriod]);
+
 
     return (
         <div className="historic-rates-page">
-            <div><Button>1Y</Button><Button>1M</Button></div>
+            <Button onClick={() => setTimePeriod(TIME_LAPSE.THREE_MTH)}>Three Month</Button>
+            <Button onClick={() => setTimePeriod(TIME_LAPSE.MTH)}>Month</Button>
             <br />
             <div className="grid">
                 <Line data={data} />
