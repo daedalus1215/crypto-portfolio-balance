7a01c0eff0dfbdff3c7b676fd4283356ca65cc58
larry adams <ladams1776@yahoo.com>
2021-04-04 13:54:15 -0400

    Move Topbar, we might create hook related to it.

--- frontend/src/topBar/TopBar.js
+++ frontend/src/topBar/TopBar.js
@@ -2,8 +2,12 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { withRouter } from "react-router-dom";
+import useGetPortfolios from "../portfolios/useGetPortfolios";
+
 function TopBar({ location }) {
     const { pathname } = location;
+    const portfolios = useGetPortfolios();
+
     return (
         <Navbar bg="primary" expand="lg" variant="dark">
             <Navbar.Brand href="#home">Crypto Currency Portfolio App</Navbar.Brand>
@@ -14,7 +18,7 @@ function TopBar({ location }) {
                         href="/">
                         Bitcoin Historical Rate
                     </Nav.Link>
-                    <Nav.Link 
+                    <Nav.Link
                         href="/historicBtcPurchases"
                         active={pathname.includes("/historicBtcPurchases")}>
                         Btc Purchases
@@ -34,6 +38,11 @@ function TopBar({ location }) {
                         active={pathname.includes("/combine-portfolio")}>
                         Combine Portfolio
                     </Nav.Link>
+                    {portfolios?.map(p => (<Nav.Link
+                        href={"/" + p.code}
+                        active={pathname.includes("/" + p.code)}>
+                        {p.name}
+                    </Nav.Link>))}
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
