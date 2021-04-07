5a7bfae62f2529cab155df02fb4cc4ad1e023fa7
larry adams <ladams1776@yahoo.com>
2021-04-07 08:55:17 -0400

    Fix fetchAssetActivity, it now works.

--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -30,7 +30,7 @@ export const fetchEtherHistory = async (setHData) => await axios
     })
     .catch(err => err);
 
-    //@TODO: Swap this out so that the backend is dynamic and we are not hitting specific endpoint
+//@TODO: Swap this out so that the backend is dynamic and we are not hitting specific endpoint
 export const fetchAssetHistory = async (code, setHData) => await axios
     .get(`http://localhost:8081/api/history/${code.toUpperCase()}`)
     .then(async resp => setHData(await resp.data))
@@ -41,7 +41,26 @@ export const fetchAssetHistory = async (code, setHData) => await axios
 
 export const fetchAssetActivity = async (code, setActivity) => await axios
     .get(`http://localhost:8081/api/activity/${code}`)
-    .then(async resp => setActivity(await resp.data))
+    .then(async resp => {
+        const portfolio = await resp.data;
+
+        //@TODO: Might need to do something with isNaN here
+        const fiatInvestment = portfolio
+            .map(p => parseFloat(p.order))
+            .reduce((sum, order) => order + sum, 0);
+
+        //@TODO: Might need to do something with isNaN here
+        const totalAmountOfAsset = portfolio
+            .map(p => parseFloat(p.amount))
+            .reduce((p1, p2) => p1 + p2, 0)
+            .toFixed(9);
+
+        setActivity({
+            portfolio,
+            fiatInvestment,
+            totalAmountOfAsset
+        });
+    })
     .catch(err => {
         console.log(`issue with fetching asset activity: ${err}`);
         return err;
