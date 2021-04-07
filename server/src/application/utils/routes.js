17b605da1e790e83fd17b44a01d2c480df6e810a
larry adams <ladams1776@yahoo.com>
2021-04-06 23:02:18 -0400

    register new request handler with routes.js

--- server/src/application/utils/routes.js
+++ server/src/application/utils/routes.js
@@ -1,10 +1,11 @@
 const getCryptoPriceAction = require('../actions/getCryptoPriceAction');
+const getCryptoProfileActivity = require('../actions/getCryptoProfileActivity');
 
 const routes = (app, key) => {
     const getCryptoPriceRequestHandler = getCryptoPriceAction(key);
     app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
-    app.head('/status', (req, res) => { res.status(200).end(); });
     app.get('/api/history/:currency', getCryptoPriceRequestHandler)
+    app.get('/api/activity/:code', getCryptoProfileActivity)
 }
 
 module.exports = routes;
