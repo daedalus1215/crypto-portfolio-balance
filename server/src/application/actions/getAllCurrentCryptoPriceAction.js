b3c9a3c1dbf8b2e8fdd73c3a9b4b95455cb7a8ad
larry adams <ladams1776@yahoo.com>
2021-05-07 23:11:06 -0400

    Spike in POC with getAllCurrentCryptoPriceAction to get it to save into the collections dynamically

--- server/src/application/actions/getAllCurrentCryptoPriceAction.js
+++ server/src/application/actions/getAllCurrentCryptoPriceAction.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const getGenericCryptoModel = require('../../infrastructure/models/schemas/getGenericCryptoModel');
 const portfolio = require('../../portfolio.json');
 const key = process.env.X_CMC_PRO_API_KEY;
 const COIN_MARKET_CAP_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
@@ -14,20 +15,36 @@ module.exports = (req, response) => {
             const currentCryptos = await resp.data;
 
             const timestamp = currentCryptos.status.timestamp;
+            console.log('timestamp', timestamp)
             const g = portfolio.map(p => {
                 return currentCryptos.data.filter(c => {
                     if (p.code.toUpperCase() === c.symbol) {
                         return true;
                     }
                     return false;
-                }).map(d => {
-                    return {
-                        symbol: `${d.symbol}/USD`,
-                        date: d.last_updated,
-                        close: d.quote.USD.price
-                    }
+                }).map(async d => {
+                    const model = getGenericCryptoModel(d.symbol);
+                    const saveable = new model();
+                    saveable.toObject();
+                    saveable.symbol = d.symbol;
+                    saveable.date = d.last_updated;
+                    saveable.close = d.quote.USD.price;
+
+                    const z = await saveable.save((err, doc) => { console.log('error:', err); return doc })
+                    // .catch(e => {
+                    //     console.log('error: ', e)
+                    // });
+                    console.log('saved', z);
+                    return z;
+                    // return {
+                    //     symbol: d.symbol,
+                    //     date: d.last_updated,
+                    //     close: d.quote.USD.price
+                    // }
                 });
             });
+
+
 
             console.log('xxxxx', g);
             response.send(resp.data);
