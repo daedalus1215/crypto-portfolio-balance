2d3a4b9eb34394a07d8c1fc31dc0e4c73ffacfcf
larry adams <ladams1776@yahoo.com>
2021-05-09 10:59:25 -0400

    Update getGenericCryptoModel Move schema outside of function, to lock in the schema creation the first time. this fixes an issue we kept seeing pop up

--- server/src/infrastructure/models/schemas/getGenericCryptoModel.js
+++ server/src/infrastructure/models/schemas/getGenericCryptoModel.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
-const getGenericCryptoModel = code => {
-    return mongoose.model(`${code}s`, new mongoose.Schema({
-        "unix": String,
-        "date": String,
-        "symbol": String,
-        "open": String,
-        "high": String,
-        "low": String,
-        "close": String,
-        "Volume BTC": String,
-        "Volume USD": String
-    }));
-}
+const schema = new mongoose.Schema({
+    "unix": String,
+    "date": String,
+    "symbol": String,
+    "open": String,
+    "high": String,
+    "low": String,
+    "close": String,
+    "Volume BTC": String,
+    "Volume USD": String
+});
 
+const getGenericCryptoModel = code => {
+    return mongoose.model(`${code}s`, schema);
+}
 module.exports = getGenericCryptoModel;
