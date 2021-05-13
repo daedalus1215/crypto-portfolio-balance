4c978c2837f905ee15ffe05058a5db30e9324149
larry adams <ladams1776@yahoo.com>
2021-05-12 23:13:18 -0400

    update getCryptoProfileActivity so that it does not relies on the .json and it relies on mongo

--- server/src/application/actions/getCryptoProfileActivity.js
+++ server/src/application/actions/getCryptoProfileActivity.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+const ActivityModel = require('../../infrastructure/models/schemas/ActivityModel');
+const { ASC } = require('../../infrastructure/models/utils/constants.js');
 
 const getCryptoProfileActivity = (req, res) => {
-    console.log('getCryptoProfileActivity');
     const { code } = req.params;
-    fs.readFile('src/temp/' + code + '.json', (err, data) => {
-        if (err) throw new Error(`Problem: ${err}`);
-        res.status(200).jsonp(JSON.parse(data)).end();
+
+    ActivityModel.find({ Coin: code }, {}, { sort: { unix: ASC } }, (err, doc) => {
+        console.log('err: ', err);
+        res.jsonp(doc)
     });
 };
 
