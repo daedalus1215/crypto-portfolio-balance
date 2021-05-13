8d321a90eb93fc962e8b8e94e6d1d20979c4b640
larry adams <ladams1776@yahoo.com>
2021-05-12 23:13:50 -0400

    update routes No longer wrapping RH, no need since we are not using nomics.

--- server/src/application/utils/routes.js
+++ server/src/application/utils/routes.js
@@ -1,12 +1,12 @@
 const getCryptoPriceAction = require('../actions/getCryptoPriceAction');
 const getCryptoProfileActivity = require('../actions/getCryptoProfileActivity');
-const getAllCurrentCryptoPriceAction = require('../actions/getAllCurrentCryptoPriceAction');
+const getAllCurrentCryptoPriceAction = require('../actions/pollAllCurrentCryptoPriceAction');
 
 const routes = (app, key) => {
-    const getCryptoPriceRequestHandler = getCryptoPriceAction(key);
+    // const getCryptoPriceRequestHandler = getCryptoPriceAction(key);
     app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
     app.get('/api/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
-    app.get('/api/history/:currency', getCryptoPriceRequestHandler);
+    app.get('/api/history/:currency', getCryptoPriceAction);
     app.get('/api/activity/:code', getCryptoProfileActivity);
     app.get('/api/getAllCurrentCryptoPrice', getAllCurrentCryptoPriceAction);
 }
