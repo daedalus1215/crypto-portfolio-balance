47d9d9201aed2213518e7be4bceb3df8737f541f
larry adams <ladams1776@yahoo.com>
2021-05-13 17:09:17 -0400

    update getCryptoProfileActivity to accommodate db shift

--- server/src/application/actions/getCryptoProfileActivity.js
+++ server/src/application/actions/getCryptoProfileActivity.js
@@ -4,7 +4,7 @@ const { ASC } = require('../../infrastructure/models/utils/constants.js');
 const getCryptoProfileActivity = (req, res) => {
     const { code } = req.params;
 
-    ActivityModel.find({ Coin: code }, {}, { sort: { unix: ASC } }, (err, doc) => {
+    ActivityModel.find({ Coin: code }, {}, { sort: { Unix: ASC } }, (err, doc) => {
         console.log('err: ', err);
         res.jsonp(doc)
     });
