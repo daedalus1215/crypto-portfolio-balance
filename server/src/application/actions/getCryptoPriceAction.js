614427bd4b6f62062ce7fa60d890f526cd879d46
larry adams <ladams1776@yahoo.com>
2021-05-28 21:08:45 -0400

    remove white line

--- server/src/application/actions/getCryptoPriceAction.js
+++ server/src/application/actions/getCryptoPriceAction.js
@@ -1,14 +1,10 @@
 const getGenericCryptoModel = require('../../infrastructure/models/schemas/getGenericCryptoModel');
 const { ASC } = require('../../infrastructure/models/utils/constants');
 
-
 const getCryptoPriceAction = (req, response) => {
     const currency = req.params.currency;
-
     const model = getGenericCryptoModel(currency.toLowerCase());
-
     model.find({}, {}, { sort: { Unix: ASC } }, async (err, doc) => {
-        
         response.jsonp(await doc)
     });
 };
