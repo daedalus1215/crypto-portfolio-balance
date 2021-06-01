6dab466ff723bf056c838bdd6a8b8c1ff4201e85
larry adams <ladams1776@yahoo.com>
2021-06-01 19:47:57 -0400

    update getCryptoProfileActivity RH remove all the consoling

--- server/src/application/actions/getCryptoProfileActivity.js
+++ server/src/application/actions/getCryptoProfileActivity.js
@@ -3,14 +3,14 @@ const { ASC } = require('../../infrastructure/models/utils/constants.js');
 
 const axios = require('axios');
 const getGenericCryptoModel = require('../../infrastructure/models/schemas/getGenericCryptoModel');
-const portfolio = require('../../portfolio.json');
+const portfolio = require('../../temp/portfolio.json');
 const key = process.env.X_CMC_PRO_API_KEY;
 const COIN_MARKET_CAP_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
 
 const getCryptoProfileActivity = (req, res) => {
-    console.log('getAllCurrentCryptoPriceAction');
+    // console.log('getAllCurrentCryptoPriceAction');
     const url = COIN_MARKET_CAP_URL;
-    console.log('portfolio', portfolio)
+    // console.log('portfolio', portfolio)
     axios.defaults.headers.common = { "Accept": "application/json", "X-CMC_PRO_API_KEY": key };
     
     axios
@@ -43,12 +43,12 @@ const getCryptoProfileActivity = (req, res) => {
                     });
             })
             // response.send({ 'Updated with': currentCryptos })
-            console.log('returned from coinmarket', currentCryptos)
+            // console.log('returned from coinmarket', currentCryptos)
         })
         .then(async () => {
             const { code } = req.params;
             ActivityModel.find({ Coin: code }, {}, { sort: { Unix: ASC } }, (err, doc) => {
-                console.log('err: ', err);
+                // console.log('err: ', err);
                 res.jsonp({
                     latest:true,
                     items: doc
@@ -56,11 +56,11 @@ const getCryptoProfileActivity = (req, res) => {
             });
         })
         .catch(err => {
-            console.log('err: ', err);
+            // console.log('err: ', err);
 
             const { code } = req.params;
             ActivityModel.find({ Coin: code }, {}, { sort: { Unix: ASC } }, (err, doc) => {
-                console.log('err: ', err);
+                // console.log('err: ', err);
                 res.jsonp({ 
                     latest: false,
                     items: doc
