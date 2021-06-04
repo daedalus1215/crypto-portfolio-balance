1c50db34e01bfc803624bc4ceff57b6124123f57
larry adams <ladams1776@yahoo.com>
2021-06-03 21:52:48 -0400

    update getCryptoProfileActivity RH just have it grab the acitivties. No need to hit remote api anymore, since Home will do that with pollAllCurrentCrypto

--- server/src/application/actions/getCryptoProfileActivity.js
+++ server/src/application/actions/getCryptoProfileActivity.js
@@ -11,41 +11,41 @@ const getCryptoProfileActivity = (req, res) => {
     // console.log('getAllCurrentCryptoPriceAction');
     const url = COIN_MARKET_CAP_URL;
     // console.log('portfolio', portfolio)
-    axios.defaults.headers.common = { "Accept": "application/json", "X-CMC_PRO_API_KEY": key };
+    // axios.defaults.headers.common = { "Accept": "application/json", "X-CMC_PRO_API_KEY": key };
     
-    axios
-        .get(url)
-        .then(async resp => {
-            const currentCryptos = await resp.data;
+    // axios
+    //     .get(url)
+    //     .then(async resp => {
+    //         const currentCryptos = await resp.data;
 
-            const timestamp = currentCryptos.status.timestamp;
-            const date = timestamp.match(/\d{4}\-\d{2}\-\d{2}/g)[0] + ' 00:00:00'; // format date proper
+    //         const timestamp = currentCryptos.status.timestamp;
+    //         const date = timestamp.match(/\d{4}\-\d{2}\-\d{2}/g)[0] + ' 00:00:00'; // format date proper
 
-            portfolio.map(p => {
-                return currentCryptos.data
-                    .filter(c => p.code.toUpperCase() === c.symbol)
-                    .map(async currentCryptoInPortfolio => {
-                        const model = getGenericCryptoModel(currentCryptoInPortfolio.symbol);
+        //     portfolio.map(p => {
+        //         return currentCryptos.data
+        //             .filter(c => p.code.toUpperCase() === c.symbol)
+        //             .map(async currentCryptoInPortfolio => {
+        //                 const model = getGenericCryptoModel(currentCryptoInPortfolio.symbol);
 
-                        return await model.updateOne(
-                            { Date: `${date}` },
-                            {
-                                Symbol: currentCryptoInPortfolio.symbol,
-                                Date: date,
-                                Close: currentCryptoInPortfolio.quote.USD.price,
-                                Unix: new Date(timestamp).getTime(),
-                                Open: "",
-                                Low: "",
-                                High: "",
-                                Volume: "",
-                            },
-                            { upsert: true });
-                    });
-            })
-            // response.send({ 'Updated with': currentCryptos })
-            // console.log('returned from coinmarket', currentCryptos)
-        })
-        .then(async () => {
+        //                 return await model.updateOne(
+        //                     { Date: `${date}` },
+        //                     {
+        //                         Symbol: currentCryptoInPortfolio.symbol,
+        //                         Date: date,
+        //                         Close: currentCryptoInPortfolio.quote.USD.price,
+        //                         Unix: new Date(timestamp).getTime(),
+        //                         Open: "",
+        //                         Low: "",
+        //                         High: "",
+        //                         Volume: "",
+        //                     },
+        //                     { upsert: true });
+        //             });
+        //     })
+        //     response.send({ 'Updated with': currentCryptos })
+        //     // console.log('returned from coinmarket', currentCryptos)
+        // })
+        // .then(async () => {
             const { code } = req.params;
             ActivityModel.find({ Coin: code }, {}, { sort: { Unix: ASC } }, (err, doc) => {
                 // console.log('err: ', err);
@@ -54,19 +54,19 @@ const getCryptoProfileActivity = (req, res) => {
                     items: doc
                 });
             });
-        })
-        .catch(err => {
-            // console.log('err: ', err);
+        // })
+        // .catch(err => {
+        //     // console.log('err: ', err);
 
-            const { code } = req.params;
-            ActivityModel.find({ Coin: code }, {}, { sort: { Unix: ASC } }, (err, doc) => {
-                // console.log('err: ', err);
-                res.jsonp({ 
-                    latest: false,
-                    items: doc
-                });
-            });
-        });
+        //     const { code } = req.params;
+        //     ActivityModel.find({ Coin: code }, {}, { sort: { Unix: ASC } }, (err, doc) => {
+        //         // console.log('err: ', err);
+        //         res.jsonp({ 
+        //             latest: false,
+        //             items: doc
+        //         });
+        //     });
+        // });
 
     /// previous
 
