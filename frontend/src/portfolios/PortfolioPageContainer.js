8dc80ba4184649ea739ebd1f0bdcda36f27a2605
larry adams <ladams1776@yahoo.com>
2021-06-05 17:59:37 -0400

    Make PortfolioPageContainer have more orchestration Moved it out of PortfolioPage

--- frontend/src/portfolios/PortfolioPageContainer.js
+++ frontend/src/portfolios/PortfolioPageContainer.js
@@ -1,10 +1,38 @@
-import react from 'react';
-import HistoricBtcPurchases from '../historicBtcPurchases/HistoricBtcPurchases';
+import React from 'react';
+import { connect } from 'react-redux';
 import PortfolioPage from './PortfolioPage';
+import HistoricBtcPurchases from '../historicBtcPurchases/HistoricBtcPurchases';
+import HistoricRatesPage from '../historicRatesPages/HistoricRatesPage';
+import { setPortfolioList } from '../actionCreators';
+import { getPortfolioList } from '../requests';
+import './PortfolioPage.css';
 
-const PortfolioPageContainer = ({ portfolioOfAsset }) => <div className="PortfolioPageContainer">
-    <PortfolioPage portfolioOfAsset={portfolioOfAsset} />
-    {/* <HistoricBtcPurchases /> */}
-</div>
 
-export default PortfolioPageContainer;
+
+const PortfolioPageContainer = ({ portfolioOfAsset, setList, portfolioList }) => {
+
+    React.useEffect(() => {
+        getPortfolioList(setList);
+    }, []);
+
+    console.log('portfolio list from redux', portfolioList)
+
+    return <div className="whole-page">
+
+        <PortfolioPage portfolioOfAsset={portfolioOfAsset} />
+
+        <div className="hs-page">
+            <HistoricBtcPurchases code={portfolioOfAsset.code} color={portfolioOfAsset.color} />
+        </div>
+        <div className="hr-page">
+            <HistoricRatesPage code={portfolioOfAsset.code} color={portfolioOfAsset.color} />
+        </div>
+    </div>
+}
+
+
+const mapStateToProps = state => {
+    return { portfolioList: state?.portfolioList }
+};
+
+export default connect(mapStateToProps, { setList: setPortfolioList })(PortfolioPageContainer);
