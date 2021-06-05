6cd0ba748c1c516c452e476387bb3a9cc82170ab
larry adams <ladams1776@yahoo.com>
2021-06-05 17:58:50 -0400

    Make HistoricRatesPAge match other portfolio grid style

--- frontend/src/historicRatesPages/HistoricRatesPage.js
+++ frontend/src/historicRatesPages/HistoricRatesPage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { fetchCryptoHistory } from "../requests";
 import { Line } from "react-chartjs-2";
 import Button from "react-bootstrap/esm/Button";
-import "./HistoricRatesPage.css";
 
 const TIME_LAPSE = {
     YR: "YR",
@@ -24,6 +23,11 @@ const HistoricRatesPage = ({ code, color }) => {
         let labels = btcHistory.map(btc => btc.Date);
         let cryptoData = btcHistory.map(btc => btc.Close);
 
+        if (timePeriod === TIME_LAPSE.WEEK) {
+            cryptoData = cryptoData.splice(cryptoData.length - 10, cryptoData.length - 1);
+            labels = labels.splice(labels.length - 10, labels.length - 1);
+        }
+
         if (timePeriod === TIME_LAPSE.MTH) {
             cryptoData = cryptoData.splice(cryptoData.length - 34, cryptoData.length - 1);
             labels = labels.splice(labels.length - 34, labels.length - 1);
@@ -33,6 +37,12 @@ const HistoricRatesPage = ({ code, color }) => {
             cryptoData = cryptoData.splice(cryptoData.length - 90, cryptoData.length - 1);
             labels = labels.splice(labels.length - 90, labels.length - 1);
         }
+
+        if (timePeriod === TIME_LAPSE.YR) {
+            cryptoData = cryptoData.splice(cryptoData.length - 150, cryptoData.length - 1);
+            labels = labels.splice(labels.length - 150, labels.length - 1);
+        }
+
 
         // console.log('labels', labels)
         // console.log('cryptoData', cryptoData);
@@ -54,19 +64,23 @@ const HistoricRatesPage = ({ code, color }) => {
     const currentMarketValue = btcHistory[btcHistory.length - 1]?.Close;
 
     return (
-        <div className="historic-rates-page">
-            <div className="title-container">
-                <div className="title">
-                    {/* <p>Invested: ${fiatInvestment?.toFixed(2)}</p>
-                    <p>Valued at: ${totalV}</p> */}
+        <div className="p-page">
+            <div className="portfolio-page">
+                <div className="title-container">
+                    <div className="buttons">
+                        <Button onClick={() => setTimePeriod(TIME_LAPSE.ALL)}>All</Button>
+                        <Button onClick={() => setTimePeriod(TIME_LAPSE.YR)}>Year</Button>
+                        <Button onClick={() => setTimePeriod(TIME_LAPSE.THREE_MTH)}>Three Month</Button>
+                        <Button onClick={() => setTimePeriod(TIME_LAPSE.MTH)}>Month</Button>
+                        <Button onClick={() => setTimePeriod(TIME_LAPSE.WEEK)}>Week</Button>
+                    </div>
+                    <div className="title">
                     <p>CM Value: {currentMarketValue}</p>
+                    </div>
                 </div>
-            </div>
-            <Button onClick={() => setTimePeriod(TIME_LAPSE.THREE_MTH)}>Three Month</Button>
-            <Button onClick={() => setTimePeriod(TIME_LAPSE.MTH)}>Month</Button>
-            <br />
-            <div className="grid">
-                <Line data={data} />
+                <div className="portfolio-grid">
+                    <Line data={data} />
+                </div>
             </div>
         </div>
     );
