e96cce33a9a7f00e946a73d17f3b48163c195248
larry adams <ladams1776@yahoo.com>
2021-06-19 10:48:04 -0400

    Create the cryptoHistoryReducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,11 +1,26 @@
+import { FETCH_CRYPTO_HISTORY_ERROR, FETCH_CRYPTO_HISTORY_RESPONSE, PORTFOLIO_LIST } from "../actionCreators";
+
 export const portfolioListReducer = (state, action) => {
-    console.log('reducer!', action.payload);
     switch (action.type) {
         case "PORTFOLIO_LIST":
             return {
+                ...state,
                 portfolioList: action.payload
             };
         default:
             return state;
+    }
+};
+
+export const cryptoHistoryReducer = (state, action) => {
+    switch (action.type) {
+        case FETCH_CRYPTO_HISTORY_ERROR:
+        case FETCH_CRYPTO_HISTORY_RESPONSE:
+            return {
+                ...state,
+                cryptoHistory: action.payload,
+            }
+        default:
+            return state
     }
 }
