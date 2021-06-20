a7359c647438c89ddd9d2a8d468ea3c8b2889327
larry adams <ladams1776@yahoo.com>
2021-06-20 15:07:50 -0400

    Clean up fetchCryptoHistory

--- frontend/src/actionCreators/index.js
+++ frontend/src/actionCreators/index.js
@@ -1,6 +1,6 @@
+import axios from "axios";
 
 export const PORTFOLIO_LIST = 'PORTFOLIO_LIST';
-
 export const FETCH_CRYPTO_HISTORY_RESPONSE = 'FETCH_CRYPTO_HISTORY_RESPONSE';
 export const FETCH_CRYPTO_HISTORY_ERROR = 'FETCH_CRYPTO_HISTORY_ERROR';
 
@@ -11,14 +11,15 @@ export const setPortfolioList = portfolios => {
     }
 };
 
-/**
+/** 
  * Fetch the historical records for a specific crypto.
  * @param {String} code the code representation of the crypto asset: e.g. Bitcoin's would be 'btc', Ethereum's would be 'eth', Stellar Lumens would be 'xlm', etc.
  * @returns 
  */
-export const fetchCryptoHistory = code => async dispatch => {
-    await axios
-        .get(`http://localhost:8081/api/history/${code}`)
-        .then(async resp => dispatch({ type: 'FETCH_CRYPTO_HISTORY_RESPONSE', payload: await resp.data }))
-        .catch(err => dispatch({ type: 'FETCH_CRYPTO_HISTORY_ERROR', payload: console.log(err) }));
-};
+export const fetchCryptoHistory = code => async dispatch => await axios
+    .get(`http://localhost:8081/api/history/${code.toUpperCase()}`)
+    .then(async resp => {
+        const payload = await resp.data;
+        dispatch({ type: FETCH_CRYPTO_HISTORY_RESPONSE, payload })
+    })
+    .catch(err => dispatch({ type: FETCH_CRYPTO_HISTORY_ERROR, payload: console.log(err) }))
