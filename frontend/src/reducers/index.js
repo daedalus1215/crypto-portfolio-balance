910025cec9df854210f566cbc3d54c0348de0d63
larry adams <ladams1776@yahoo.com>
2021-06-20 22:46:42 -0400

    clean up reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,24 +1,24 @@
 import { FETCH_CRYPTO_HISTORY_ERROR, FETCH_CRYPTO_HISTORY_RESPONSE, PORTFOLIO_LIST } from "../actionCreators";
 
-export const portfolioListReducer = (state, action) => {
+export const portfolioListReducer = (state = [], action) => {
     switch (action.type) {
-        case "PORTFOLIO_LIST":
+        case PORTFOLIO_LIST:
             return {
                 ...state,
-                portfolioList: action.payload
+                assets: action.payload
             };
         default:
             return state;
     }
 };
 
-export const cryptoHistoryReducer = (state, action) => {
+export const cryptoHistoryReducer = (state = [], action) => {
     switch (action.type) {
-        case FETCH_CRYPTO_HISTORY_ERROR:
         case FETCH_CRYPTO_HISTORY_RESPONSE:
+        case FETCH_CRYPTO_HISTORY_ERROR:
             return {
                 ...state,
-                cryptoHistory: action.payload,
+                asset: action.payload,
             }
         default:
             return state
