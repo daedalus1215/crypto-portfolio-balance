9eb31f6c5d0c14034bc7a934b29b2ae42d5f0f01
la <lfadms@yahoo.com>
2021-06-21 23:40:15 -0400

    Update App to use redux for portfolio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import { fetchPortfolioList } from './actionCreators';
 import { createBrowserHistory as createHistory } from 'history';
 import TopBar from './topBar/TopBar';
-import useGetPortfolios from './hooks/useGetPortfolios';
 import Home from './home/Home';
 import PortfolioPageContainer from './portfolios/PortfolioPageContainer';
 import './App.css';
 
 const history = createHistory();
+
+const selectPortfolioList = createSelector(
+  state => state?.portfolio,
+  item => item?.assets || []
+);
+
+const useFetchPortfolioList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPortfolioList());
+  }, []);
+}
 
 const App = () => {
   window.Chart.defaults.global.defaultFontFamily = `
@@ -16,15 +30,17 @@ const App = () => {
   "Fira Sans", "Droid Sans", "Helvetica Neue", 
   sans-serif`;
 
-  const portfolios = useGetPortfolios();
+  useFetchPortfolioList();
+  const portfolios = useSelector(selectPortfolioList);
 
   return (
     <div className="App">
       <Router history={history}>
         <TopBar />
         <Route path="/" exact render={props => <Home portfolios={portfolios} />} />
-        {portfolios.map(p => <Route path={"/" + p.code} exact render={props => <PortfolioPageContainer portfolioOfAsset={p} {...props} />} key={p.code}/>)}
-        {/* <Route path="/combine-portfolio" exact component={CombinePortfolio} /> */}
+        {portfolios.map(p => <Route path={"/" + p.code} exact render={props =>
+          <PortfolioPageContainer portfolioOfAsset={p} {...props} />} key={p.code}
+        />)}
       </Router>
     </div>
   );
