8540b6c1a6e349ef1b6953d44ffa10b580f71844
la <lfadms@yahoo.com>
2021-06-29 22:19:07 -0400

    create fetchPortfolioByCode

--- frontend/src/actionCreators/portfolioActionCreators.js
+++ frontend/src/actionCreators/portfolioActionCreators.js
@@ -1,9 +1,16 @@
-import { PORTFOLIO_LIST } from ".";
+import { PORTFOLIO_BY_CODE, PORTFOLIO_LIST } from ".";
 const portfolios = require('../temp/portfolio.json');
 
 export const fetchPortfolioList = () => dispatch => {
     //@TODO: should throw error here if portfolios is not set. Would help if temp file was missing. 
     dispatch({
         type: PORTFOLIO_LIST, payload: portfolios
+    });
+};
+
+export const fetchPortfolioByCode = code => dispatch => {
+    //@TODO: should throw error here if portfolios is not set. Would help if temp file was missing. 
+    dispatch({
+        type: PORTFOLIO_BY_CODE, payload: portfolios.filter(p => p.code === code)[0],
     });
 };
