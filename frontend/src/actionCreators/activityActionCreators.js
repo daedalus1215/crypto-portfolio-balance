a9037b2fc1ef637795d27f8160ed8f08e66b15e7
la <lfadms@yahoo.com>
2021-06-29 22:18:38 -0400

    Create fetchActivityByCode

--- frontend/src/actionCreators/activityActionCreators.js
+++ frontend/src/actionCreators/activityActionCreators.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { FETCH_ALL_ACTIVITY_ERROR, FETCH_ALL_ACTIVITY_RESPONSE } from ".";
+import {
+    FETCH_ACTIVITY_BY_CODE_ERROR,
+    FETCH_ACTIVITY_BY_CODE_RESPONSE,
+    FETCH_ALL_ACTIVITY_ERROR,
+    FETCH_ALL_ACTIVITY_RESPONSE
+} from ".";
 
 /** 
 * Fetch all portfolio asset
@@ -13,3 +18,11 @@ export const fetchAllActivity = () => async dispatch => await axios
         dispatch({ type: FETCH_ALL_ACTIVITY_RESPONSE, payload })
     })
     .catch(err => dispatch({ type: FETCH_ALL_ACTIVITY_ERROR, payload: console.log(err) }))
+
+export const fetchActivityByCode = code => async dispatch => await axios
+    .get(`http://localhost:8081/api/activity/${code}`)
+    .then(async resp => {
+        const payload = await resp.data.items;
+        dispatch({ type: FETCH_ACTIVITY_BY_CODE_RESPONSE, payload })
+    })
+    .catch(err => dispatch({ type: FETCH_ACTIVITY_BY_CODE_ERROR, payload: console.log(err) }))
