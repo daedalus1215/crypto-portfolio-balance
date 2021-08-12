4eaefc735c562ec15a97287c5cd8db6507105a40
lala <ladams1776@yahoo.com>
2021-08-12 19:57:18 -0400

    Remove unused import from Home

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import useFetchInstrument from "../hooks/useFetchInstrumentHistory";
-import { fetchCurrentInstrumentPrices } from '../requests';
 import PortfolioChart from './PortfolioChart';
 import PortfolioGrid from './PortfolioGrid';
 import './Home.css';
@@ -10,6 +9,7 @@ const Home = () => {
     //@TODO: Got to change this, we should not be depending on btc as a financial instrument.
     //@TODO: I just need the full date range and I picked anyone of the financial instruments.
     useFetchInstrument('btc');
+
 
     return <div className="home-page">
         <div className="home-page-container">
